77b01dac7636e493770ec862f75e5dffbb8cc732
root <6281@holbertonstudents.com>
2023-05-31 01:17:43 -0700

    Fix: delete one statements in the file 7-islower

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/pyhton3
 def islower(c):
-    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+    if ord(c) in range(ord('a'), ord('z')+1):
         return True
     else:
         return False
