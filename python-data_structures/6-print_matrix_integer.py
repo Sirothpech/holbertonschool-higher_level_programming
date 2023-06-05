8806c9e2b23a0550603f413c1e6fb4a32f3ec23d
Sirothpech <christophe.ngan@gmail.com>
2023-06-05 18:44:33 +0200

    Fix: delete the len(row) replace by row

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,8 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return
     for row in matrix:
-        for i in range(len(row)):
-            print('{:d}'.format(row[i]), end ='')
-            if i < len(row) - 1:
-                print(end= ' ')
+        for i in row:
+            print('{:d}'.format(i), end ="")
+            if i != row[-1]:
+                print(end= " ")
         print()
