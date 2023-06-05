cc73d19e34b12bd1e96f3feae3b644ac38930117
Sirothpech <christophe.ngan@gmail.com>
2023-06-05 18:19:27 +0200

    FIx: add the statement if matrix is none

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return None
     for row in matrix:
         for i in range(len(row)):
             print('{:d}'.format(row[i]), end ='')
