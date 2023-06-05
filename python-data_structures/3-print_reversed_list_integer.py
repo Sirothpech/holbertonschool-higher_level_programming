b0f079ce9747a78b4520d7c385eb8c93a0b53219
Sirothpech <christophe.ngan@gmail.com>
2023-06-05 15:22:24 +0200

    Fix: change the statement

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     if my_list is None:
         return None
-    for num in range(len(my_list), 0, - 1):
-        print("{:d}".format(num))
+    for num in range(len(my_list)-1, -1, -1):
+        print("{:d}".format(my_list[num]))
