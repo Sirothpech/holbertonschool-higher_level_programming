e7d3937dc8eeaa41181353db4f28ec5d6263fdb5
Sirothpech <christophe.ngan@gmail.com>
2023-06-05 23:14:57 +0200

    Fix: use the range

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None   
     max_value = my_list[0]
-    for num in my_list[1:]:
-        if num > max_value:
-            max_value = num   
+    for num in range(1, len(my_list)):
+        if my_list[num] > max_value:
+            max_value = my_list[num]   
     return max_value
