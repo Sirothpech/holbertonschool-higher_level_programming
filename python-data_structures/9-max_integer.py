1f22bdc5f9bd6530c3d9ad557b7c110231e60790
Sirothpech <christophe.ngan@gmail.com>
2023-06-05 23:16:43 +0200

    Fix: use the method sort()

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return None   
-    max_value = my_list[0]
-    for num in range(1, len(my_list)):
-        if my_list[num] > max_value:
-            max_value = my_list[num]   
+        return None
+    my_list.sort()
+    max_value = my_list[-1]
     return max_value
