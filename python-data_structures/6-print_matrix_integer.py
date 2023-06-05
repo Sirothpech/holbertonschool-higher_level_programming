8bd59fab33560cafcb1fe4c04d32795b95917c63
Sirothpech <christophe.ngan@gmail.com>
2023-06-05 18:14:11 +0200

    Fix: set the :d and the single quote

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
-            print("{}".format(row[i]), end ='')
+            print('{:d}'.format(row[i]), end ='')
             if i < len(row) - 1:
-                print(" ", end= '')
+                print(' ', end= '')
         print()
     return matrix
