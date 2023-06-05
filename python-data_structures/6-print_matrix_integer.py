073105182f62546de3d4a5d8a27d15f7fe8db2f5
Sirothpech <christophe.ngan@gmail.com>
2023-06-05 20:55:26 +0200

    Fix: delete the space after equal

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,5 +6,5 @@ def print_matrix_integer(matrix=[[]]):
         for i in row:
             print('{:d}'.format(i), end="")
             if i != row[-1]:
-                print(end= " ")
+                print(end=" ")
         print()
