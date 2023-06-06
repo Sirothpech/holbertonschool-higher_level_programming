f62fafeb3d28276c4386f6e75364ca4a95333da0
Sirothpech <christophe.ngan@gmail.com>
2023-06-06 13:23:25 +0200

    Add: replaces an element by another in a new list.

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list is None:
+        return
+    if search in range(0,len(my_list)):
+        my_list.insert(search, replace)
+    return my_list
