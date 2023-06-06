be6297a383ec3256ece3c8063b844427d0c17341
Sirothpech <christophe.ngan@gmail.com>
2023-06-06 21:44:49 +0200

    Fix: added the statement if none or is not str

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
     matrix = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "M": 1000, "D": 500}
     number = 0
     position = 0
