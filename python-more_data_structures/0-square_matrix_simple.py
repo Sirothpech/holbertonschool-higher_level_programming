4e5da73eb7902310282977dd1e50e839ad353bb8
Sirothpech <christophe.ngan@gmail.com>
2023-06-06 11:14:54 +0200

    Add: square value of all integers of a matrix.

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix is None:
+        return
+    result = []
+    for row in matrix:
+        new_row = []
+        for element in row:
+            squared = element ** 2  
+            new_row.append(squared)
+        result.append(new_row)
+    return result
