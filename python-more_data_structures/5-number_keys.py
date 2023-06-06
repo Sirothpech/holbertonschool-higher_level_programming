58db6622dcecb282403e0abacfe86e531593fb2e
Sirothpech <christophe.ngan@gmail.com>
2023-06-06 14:59:44 +0200

    Fix: added the else statement

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    if a_dictionary == {}:
+    if a_dictionary == "":
         return
-    return len(a_dictionary)
+    else:
+        return len(a_dictionary)
