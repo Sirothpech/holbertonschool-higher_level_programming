8bcf5ccd202766079d9f70c349fc9196a6ba5964
Sirothpech <christophe.ngan@gmail.com>
2023-06-06 13:40:43 +0200

    Fix: change my code to not modify the list initial

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,6 +2,8 @@
 def search_replace(my_list, search, replace):
     if my_list is None:
         return
-    if search in range(len(my_list)):
-        my_list.insert(search, replace)
-    return my_list
+    new_list = my_list[:]
+    for num in range(len(new_list)):
+        if new_list[num] == search:
+            new_list[num] = replace
+    return new_list
