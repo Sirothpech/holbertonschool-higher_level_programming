4278b0c8fd90eab2e8e6920c6c74c8705e2af5e9
Sirothpech <christophe.ngan@gmail.com>
2023-06-06 13:26:41 +0200

    Fix: added whitespace after ','

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     if my_list is None:
         return
-    if search in range(0,len(my_list)):
+    if search in range(0, len(my_list)):
         my_list.insert(search, replace)
     return my_list
