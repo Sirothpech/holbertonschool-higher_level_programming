e189e04f5de9b227aa2732b5ab7832d8b03a77b3
Sirothpech <christophe.ngan@gmail.com>
2023-06-07 15:31:53 +0200

    Fix: delete the whitespace

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -8,5 +8,5 @@ def safe_print_integer_err(value):
         return True
     except Exception:
         print("Exception: Unknown format code 'd' for object\
-              of type 'str'", file=sys.stderr)
+of type 'str'", file=sys.stderr)
         return False
