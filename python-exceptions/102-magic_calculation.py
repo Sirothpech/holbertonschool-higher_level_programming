c31083c7f0f674180344a0492508d191584af611
Sirothpech <christophe.ngan@gmail.com>
2023-06-09 12:24:24 +0200

    python bytecode

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
-
     for i in range(1, 3):
         try:
             if i > a:
@@ -10,8 +9,5 @@ def magic_calculation(a, b):
             result = a + b
             break
         else:
-            result += (a ** b / i)
-
-    result += a + b
-
+            result += a ** b / i
     return result
