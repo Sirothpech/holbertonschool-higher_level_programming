35b9a5e3ca498980d7a63228e5f8084ffa004dc5
Sirothpech <christophe.ngan@gmail.com>
2023-06-12 12:36:38 +0200

    init the area function

---
+++ python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Write an empty class Square that defines a square:"""
+
+
+class Square:
+    """This class represents a square.
+    Args:
+        size: private attribute
+    """
+    __size = 0
+
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        pass
+    def area(self):
+        """Calculate and return the area of the square."""
+        return self.__size ** 2
