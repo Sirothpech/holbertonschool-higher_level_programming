863449b4c566de3d95433167c2a3fe553d0e7241
Sirothpech <christophe.ngan@gmail.com>
2023-06-12 11:11:21 +0200

    Fix: init the private size on the code

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,7 +3,11 @@
 
 
 class Square:
-    """This class represents a square."""
+    """This class represents a square.
+    Args:
+        __size : Private instance attribute size.
+    """
     __size = 0
     def __init__(self,__size):
+        self.__size = __size
         pass
