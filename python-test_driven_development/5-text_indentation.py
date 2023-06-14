93f0ccbd15decadb24bb9d5f64f1fd2e61c7f48f
Sirothpech <christophe.ngan@gmail.com>
2023-06-14 09:52:05 +0200

    Add:prints a text with 2 new lines & his filetest

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Function that prints a text with 2 new lines after
+each of these characters: ., ? and :
+
+"""
+
+
+def text_indentation(text):
+    """
+    Function that prints a text with 2 new lines after
+    each of these characters: ., ? and :
+    """
+    punct = [".", "?", ":"]
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    new_line = True  # To keep track if a new line has already been printed
+    for i, char in enumerate(text):
+        if new_line and char == " ":
+            continue
+        if char in punct:
+            print(char, end="")
+            if i < len(text) - 1 and text[i+1] not in punct:
+                print("\n\n", end="")
+                new_line = True
+            else:
+                print("\n", end="")
+                new_line = False
+        else:
+            print(char, end="")
+            new_line = False
