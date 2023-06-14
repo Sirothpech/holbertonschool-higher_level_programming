59cd5cb919f271cd9095b8ae5e1bde23d2ce7804
Sirothpech <christophe.ngan@gmail.com>
2023-06-14 16:34:39 +0200

    add the new line if i the end line

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -56,7 +56,10 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
+        rectangle_str = ""
         for i in range(self.__height):
-            print("#" * self.__width)
-        else:
-            return ""
+            for j in range(self.__width):
+                rectangle_str += "#"
+            if i < self.__height - 1:
+                rectangle_str += "\n"
+        return rectangle_str
