00443b464c0aa70f38eb71901daa85a7216cf4c3
Sirothpech <christophe.ngan@gmail.com>
2023-06-14 21:33:33 +0200

    change the order of del

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -42,8 +42,8 @@ class Rectangle:
 
     def __del__(self):
         """delete the current Rectangle"""
-        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     @property
     def height(self):
