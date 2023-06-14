28856ff44eae452428b74e9994342d2f647a260d
Sirothpech <christophe.ngan@gmail.com>
2023-06-14 21:20:54 +0200

    change the order of the width and the height

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -46,20 +46,6 @@ class Rectangle:
         print("Bye rectangle...")
 
     @property
-    def width(self):
-        """retrieve the width"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """set a new value to width"""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
     def height(self):
         """retrieve the height"""
         return self.__height
@@ -72,6 +58,20 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    @property
+    def width(self):
+        """retrieve the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set a new value to width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     def area(self):
         """Calculate and return the area of the rectangle."""
