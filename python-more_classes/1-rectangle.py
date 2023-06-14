5d133619b7d74cba82a819f18c7352a6986c6dab
Sirothpech <christophe.ngan@gmail.com>
2023-06-14 14:35:14 +0200

    Fix: add the statement in the def init

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -9,6 +9,14 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
+        if not isinstance(height, int):
+            raise TypeError("width must be an integer")
+        if height < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
         self.__height = height
         self.__width = width
 
@@ -22,16 +30,16 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        self.__width = value
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
     @height.setter
     def height(self, value):
-        self.__height = value
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
