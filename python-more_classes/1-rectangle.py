8617b19fb02ac8d693a62801ba72351eada2d611
Sirothpech <christophe.ngan@gmail.com>
2023-06-14 14:39:39 +0200

    Modify the docstrings

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""Write an empty class Rectangle that defines a rectangle:"""
+"""
+Write an empty class Rectangle that defines a rectangle:
+Private instance attribute: width.
+Private instance attribute: height.
+"""
 
 
 class Rectangle:
     """
     This class represents a  rectangle.
-
     """
 
     def __init__(self, width=0, height=0):
         if not isinstance(height, int):
             raise TypeError("width must be an integer")
-        if height < 0:
+        elif height < 0:
             raise ValueError("width must be >= 0")
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        elif width < 0:
             raise ValueError("width must be >= 0")
         self.__height = height
         self.__width = width
@@ -28,7 +31,7 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
@@ -40,6 +43,6 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
