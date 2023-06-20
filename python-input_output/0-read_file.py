5026b959aa6380ef2828059c141192f2d1e4ac39
Sirothpech <christophe.ngan@gmail.com>
2023-06-20 18:49:23 +0200

    add the end statement

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,7 +5,7 @@ Write a function that reads a text file (UTF8) and prints it to stdout:
 
 
 def read_file(filename=""):
-    with open(filename, encoding="UTF8") as f:
+    with open(filename) as f:
         read_data = f.read()
-    print(read_data)
+    print(read_data, end="")
     f.closed
