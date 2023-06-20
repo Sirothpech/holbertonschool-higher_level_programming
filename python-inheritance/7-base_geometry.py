b5b510566d740d970f0eed22b3a3ca04ecf80171
Sirothpech <christophe.ngan@gmail.com>
2023-06-20 15:15:20 +0200

    add docstrings

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -5,18 +5,14 @@
 class BaseGeometry:
     """This class represents a BaseGeometry."""
     def __init__(self):
-        """Init the BaseGeometry"""
         pass
 
     def area(self):
-        """Calculate and return the area of BaseGeometry."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Check integer and validate it"""
-        if type(name) is str:
-            if type(value) is not int:
-                raise TypeError("{} must be an integer".format(name))
-            if value <= 0:
-                raise ValueError("{} must be greater than 0".format(name))
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
         return value
