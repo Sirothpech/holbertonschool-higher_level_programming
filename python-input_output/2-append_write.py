54e6212a2286f8e7da80fd778fa516c09c3194a1
Sirothpech <christophe.ngan@gmail.com>
2023-06-20 19:07:11 +0200

    Add: funct append text at the end

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Write a function that write a text file (UTF8) and prints it to stdout:
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Write a function that write a text file (UTF8) and prints it to stdout:
+    """
+    with open(filename, 'a') as f:
+        f.write(text)
+    return len(text)
