7b5f99613bdad2c1c6b1de30645b63adbf056eed
Sirothpech <christophe.ngan@gmail.com>
2023-06-20 17:49:44 +0200

    def area

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -10,3 +10,6 @@ class Square(Rectangle):
     def __init__(self, size):
         self.__size = size
         super().__init__(size, size)
+
+    def area(self):
+        return self.__size * self.__size
