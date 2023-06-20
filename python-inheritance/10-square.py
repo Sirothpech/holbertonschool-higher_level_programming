add3a99ff6a9823337af2a912b7d2da3e3cec566
Sirothpech <christophe.ngan@gmail.com>
2023-06-20 17:58:22 +0200

    super() integer validator

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -8,6 +8,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """Write an class Square"""
     def __init__(self, size):
+        super().integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
