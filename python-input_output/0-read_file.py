02c3cb07297cc70e729af10433d1aa6b80cb1d67
Sirothpech <christophe.ngan@gmail.com>
2023-06-20 18:51:06 +0200

    add docstrings

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,9 @@ Write a function that reads a text file (UTF8) and prints it to stdout:
 
 
 def read_file(filename=""):
+    """
+    Write a function that reads a text file (UTF8) and prints it to stdout:
+    """
     with open(filename) as f:
         read_data = f.read()
     print(read_data, end="")
