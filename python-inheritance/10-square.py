7c187258b911523ff1b020eb2b789c9d78ef1bb2
Sirothpech <christophe.ngan@gmail.com>
2023-06-20 17:56:47 +0200

    modify init

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -8,8 +8,8 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """Write an class Square"""
     def __init__(self, size):
-        self.__size = size
         super().__init__(size, size)
+        self.__size = size
 
     def area(self):
         return self.__size ** 2
