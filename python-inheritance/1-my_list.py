79388584230389142d51ce989adaa3ee7ad69d1e
Sirothpech <christophe.ngan@gmail.com>
2023-06-20 12:57:10 +0200

    add the docstrings

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,7 +3,11 @@
 Write a class MyList that inherits from list:
 """
 
+
 class MyList(list):
+    """
+    Write a class MyList that inherits from list:
+    """
 
     def print_sorted(self):
         """
