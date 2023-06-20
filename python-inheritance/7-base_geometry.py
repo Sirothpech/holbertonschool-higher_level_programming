43edf460cef384fb28e9eb114ef8044a921debe2
Sirothpech <christophe.ngan@gmail.com>
2023-06-20 15:44:59 +0200

    delete the init

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -4,9 +4,6 @@
 
 class BaseGeometry:
     """This class represents a BaseGeometry."""
-    def __init__(self):
-        pass
-
     def area(self):
         raise Exception("area() is not implemented")
 
