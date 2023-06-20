35028e4556b583d4789ae60c33ec6dfec4d1470f
Sirothpech <christophe.ngan@gmail.com>
2023-06-20 12:27:54 +0200

    add the docstrings

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def lookup(obj):
+    """
+    Returns the list of available attributes and methods for a given object.
+
+    Args:
+        obj: The object for which to retrieve the attributes and methods.
+
+    Returns:
+        A list containing the names of the object's attributes and methods.
+    """
     return dir(obj)
