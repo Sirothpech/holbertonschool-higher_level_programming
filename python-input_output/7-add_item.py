ac32ea082ef0ec7ecd8b099658d2d1a988378f5a
Sirothpech <christophe.ngan@gmail.com>
2023-06-22 20:37:48 +0200

    use exception

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,31 +3,17 @@
 Write a script that adds all arguments to a Python list,
 and then save them to a file:
 """
+
+
 import sys
-import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+filename = "add_item.json"
 
-def add_item(args):
-    """
-    Script to add arguments to a Python list and save them to a file
-    """
-    filename = "add_item.json"
-
-    # Load existing data from the file if it exists
-    with open(filename) as f:
-        data = json.load(f)
-
-    # Add the arguments to the list
-    data.extend(args)
-
-    # Save the updated list to the file
-    with open(filename, 'w') as f:
-        json.dump(data, f)
-
-
-if __name__ == "__main__":
-    # Remove the script name from the arguments
-    args = sys.argv[1:]
-
-    # Add the arguments to the list and save them to a file
-    add_item(args)
+try:
+    my_list = load_from_json_file(filename) + sys.argv[1:]
+except Exception:
+    my_list = sys.argv[1:]
+finally:
+    save_to_json_file(my_list, filename)
