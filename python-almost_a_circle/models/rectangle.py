b9245bc911a02725f2febc1fa717bd8918ef0108
Sirothpech <christophe.ngan@gmail.com>
2023-06-26 12:36:26 +0200

    add task 8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -77,3 +77,17 @@ class Rectangle(Base):
         """overriding the __str__ method so that it returns"""
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} -\
  {self.width}/{self.height}"
+
+    def update(self, *args):
+        """that assigns an argument to each attribute:"""
+        for i, arg in enumerate(args):
+            if i == 0:
+                self.id = arg
+            elif i == 1:
+                self.width = arg
+            elif i == 2:
+                self.height = arg
+            elif i == 3:
+                self.x = arg
+            elif i == 4:
+                self.y = arg
