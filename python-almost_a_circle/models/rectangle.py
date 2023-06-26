19b7f1f4b314f3a9a212c65755c393337f6fc81c
Sirothpech <christophe.ngan@gmail.com>
2023-06-26 11:50:10 +0200

    Add: task 5

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -65,3 +65,7 @@ class Rectangle(Base):
     def area(self):
         """Calculate and return the area of the rectangle."""
         return self.width * self.height
+
+    def display(self):
+        for i in range(self.height):
+            print("#" * self.width)
