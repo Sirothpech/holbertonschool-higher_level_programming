12e0a6f7677d46e28e5d9d91528019fc1c6b51bd
Sirothpech <christophe.ngan@gmail.com>
2023-06-26 12:00:50 +0200

    Add: overriding the str method so that it returns

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -70,3 +70,8 @@ class Rectangle(Base):
         """prints in stdout the Rectangle instance with the character #"""
         for i in range(self.height):
             print("#" * self.width)
+
+    def __str__(self):
+        
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} -\
+ {self.width}/{self.height}"
