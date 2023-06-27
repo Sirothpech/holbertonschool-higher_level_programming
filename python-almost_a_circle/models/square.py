e8f201cf28979efe8b23206a30a2442fb6aaa997
Sirothpech <christophe.ngan@gmail.com>
2023-06-27 12:00:54 +0200

    fix bug

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -42,7 +42,7 @@ class Square(Rectangle):
         """returns the dictionary representation of a Square"""
         return {
             "id": self.id,
-            "size": self.size,
+            "size": self.width,
             "x": self.x,
             "y": self.y
         }
