62f1a0448aabfbabcdece39f1cc8cc2cd0e89625
Sirothpech <christophe.ngan@gmail.com>
2023-06-27 12:19:09 +0200

    static method from json

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -33,3 +33,9 @@ class Base:
                 json_list.append(obj.to_dictionary())
         with open(filename, "w") as file:
             file.write(cls.to_json_string(json_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string == "" or json_string is None:
+            return []
+        return json.loads(json_string)
