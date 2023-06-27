c3da4df1d612eff44fd744ebeba51ccf69706e8e
Sirothpech <christophe.ngan@gmail.com>
2023-06-27 10:34:26 +0200

    add the blankline to the pycodestyle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,6 +4,7 @@ Write the class Square that inherits from Rectangle:
 """
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """initialize the square"""
     def __init__(self, size, x=0, y=0, id=None):
