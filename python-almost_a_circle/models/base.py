c6e9a9ee2feebe9cdadc2a241482b1f5e263e617
Sirothpech <christophe.ngan@gmail.com>
2023-06-27 12:26:02 +0200

    update method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -39,3 +39,14 @@ class Base:
         if json_string == "" or json_string is None:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            dummy = cls()
+        dummy.update(**dictionary)
+        return dummy
