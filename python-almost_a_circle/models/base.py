e474f7c80647402806237f157b6202c9aac0348b
Sirothpech <christophe.ngan@gmail.com>
2023-06-27 11:28:02 +0200

    update base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 This class will be the “base” of all other classes in this project.
 """
+import json
 
 
 class Base:
@@ -16,3 +17,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return []
+        return json.dumps(list_dictionaries)
+
