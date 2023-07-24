225bb542953514b74d70615f43abb34d7963bccf
Sirothpech <christophe.ngan@gmail.com>
2023-07-24 22:48:21 +0200

    Update the script with the comment (task 9)

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,14 @@
+-- Script to create the table second_table in the database hbtn_0c_0 and add multiple rows
+-- The query creates the table if it doesn't exist and inserts the specified records.
+
 CREATE TABLE IF NOT EXISTS second_table (
   id INT,
   name VARCHAR(256),
   score INT
 );
 
-INSERT INTO second_table (id, name, score)
-VALUES (1, 'John', 10),
-    (2, 'Alex', 3),
-    (3, 'Bob', 14),
-    (4, 'George', 8);
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
