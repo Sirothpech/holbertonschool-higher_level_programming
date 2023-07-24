e1a2a49277ee871600b55f8e96bb773aff4d5ea7
Sirothpech <christophe.ngan@gmail.com>
2023-07-24 23:05:07 +0200

    Add script to list records of second_table ordered by score (Task 10)

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Script to list all records of the table second_table in the database hbtn_0c_0
+-- The query retrieves all records from the second_table, ordering them by score in descending order.
+SELECT score, name FROM second_table
+ORDER BY score DESC;
