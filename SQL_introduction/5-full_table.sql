b7a003cdb7fe57f1e9b215d6318c1dda117f44a9
Sirothpech <christophe.ngan@gmail.com>
2023-07-24 17:17:14 +0200

    Update script to print full description of first_table (Task 5)

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- Script to print the full description of the table first_table in the database hbtn_0c_0
 -- Using the SHOW CREATE TABLE command to display the table definition.
-SHOW CREATE TABLE hbtn_0c_0.first_table;
+SHOW CREATE TABLE first_table;
