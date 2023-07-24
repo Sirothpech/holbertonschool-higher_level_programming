8c0a72d5ecc0915bb994be5e89a1d64d0a8260cd
Sirothpech <christophe.ngan@gmail.com>
2023-07-24 22:21:36 +0200

    Add script to insert a new row in first_table (Task 7)

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Script to insert a new row in the table first_table in the database hbtn_0c_0
+-- The query inserts a new row with id=89 and name='Best School'.
+INSERT INTO first_table (id, name)
+VALUES (89, 'Best School');
