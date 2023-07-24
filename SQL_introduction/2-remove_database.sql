f65346705624fbeeedfe69e7bbbbfd3d7b5bd636
Sirothpech <christophe.ngan@gmail.com>
2023-07-24 16:48:59 +0200

    Add script to delete hbtn_0c_0 database if it exists (Task 2)

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Script to delete the database hbtn_0c_0 if it exists in MySQL server
+-- The 'DROP DATABASE IF EXISTS' statement deletes the database only if it exists.
+DROP DATABASE IF EXISTS hbtn_0c_0;
