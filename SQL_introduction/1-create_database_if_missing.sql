c1cce93ada5edfa67b913b70b72da778c063a6eb
Sirothpech <christophe.ngan@gmail.com>
2023-07-24 16:14:54 +0200

    Add script to create the hbtn_0c_0 database if missing (Task 1)

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Script to create the database hbtn_0c_0 in MySQL server if it doesn't exist.
+-- The 'CREATE DATABASE IF NOT EXISTS' statement creates the database only if it's not already present.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
