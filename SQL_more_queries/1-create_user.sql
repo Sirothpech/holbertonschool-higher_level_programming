c1510367e71c3c653d8be4052d3c1c42eaced661
Sirothpech <christophe.ngan@gmail.com>
2023-07-25 13:51:36 +0200

    Update the task1 MySQL user 'user_0d_1' with all privileges (Task 1)

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
--- Script to create a user 'user_0d_1' if it does not exist
--- Create user 'user_0d_1' with the password 'user_0d_1_pwd' if it does not already exist
-CREATE USER IF NOT EXISTS user_0d_1
+-- Script to create the MySQL server user user_0d_1 with all privileges
+-- Create user user_0d_1 if it does not exist and Grant all privileges to user_0d_1 on all databases
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
