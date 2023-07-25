09a17c960266e6793a798598dd54d254f4b1f86c
Sirothpech <christophe.ngan@gmail.com>
2023-07-25 14:44:05 +0200

    Update use the flush on this script file1-create user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,3 +3,5 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Set all privileges.
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- Flush privileges to apply the changes immediately
+FLUSH PRIVILEGES;
