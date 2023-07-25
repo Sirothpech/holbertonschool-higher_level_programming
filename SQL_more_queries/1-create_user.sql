5bf309a41f52991cf0c42e287826708304574b4e
Sirothpech <christophe.ngan@gmail.com>
2023-07-25 14:16:02 +0200

    Delete the flush and commnt each line

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,10 +1,5 @@
 -- Script to create the MySQL server user user_0d_1 with all privileges
-
 -- Create user user_0d_1 if it does not exist and set the password to user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges to user_0d_1 on all databases and tables
+-- Set all privileges
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1';
-
--- Flush privileges to apply the changes immediately
-FLUSH PRIVILEGES;
