c45789a23f76a132941064841830a43ceb448373
Sirothpech <christophe.ngan@gmail.com>
2023-07-25 13:38:54 +0200

    Create user 'user_0d_1' if not exists (Task 1)

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Script to create a user 'user_0d_1' if it does not exist
+-- Create user 'user_0d_1' with the password 'user_0d_1_pwd' if it does not already exist
+CREATE USER IF NOT EXISTS user_0d_1
+IDENTIFIED BY 'user_0d_1_pwd';
