195877a099bee8375b63b34a5af982bf98e74ea7
Sirothpech <christophe.ngan@gmail.com>
2023-07-25 14:17:30 +0200

    Update the task 1 added localhost name after the user name

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Script to create the MySQL server user user_0d_1 with all privileges
 -- Create user user_0d_1 if it does not exist and set the password to user_0d_1_pwd
-CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Set all privileges
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
