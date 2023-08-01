69402146c0e77baea694f1840657a8d5b4a01853
Sirothpech <christophe.ngan@gmail.com>
2023-08-01 21:07:40 +0200

    Add script to list all Cities objects from the database hbtn_0e_6_usa

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Start link class City to table in database
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+
+Base = declarative_base()
+
+
+class City(Base):
+    """State class for the states table"""
+
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('state.id'), nullable=False)
