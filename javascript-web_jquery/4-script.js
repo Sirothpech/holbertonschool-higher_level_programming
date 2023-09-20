64da09986a08523637bee571ddd86c8484b38c35
Sirothpech <christophe.ngan@gmail.com>
2023-09-20 10:38:59 +0200

    Fix the task 4

--- javascript-web_jquery/4-script.js
+++ javascript-web_jquery/4-script.js
@@ -1,4 +1,3 @@
-$('#toggle_header').on('click', function () {
-  $('header').toggleClass('red');
-  $('header').toggleClass('green');
+$('DIV#toggle_header').on('click', function () {
+  $('header').toggleClass('red green');
 });
