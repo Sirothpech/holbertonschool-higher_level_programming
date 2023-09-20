e4eca1fac98e3ba044aff5cf025067bb7ab8dfe3
Sirothpech <christophe.ngan@gmail.com>
2023-09-20 11:00:25 +0200

    fix the ready document

--- javascript-web_jquery/9-script.js
+++ javascript-web_jquery/9-script.js
@@ -1,3 +1,5 @@
-$.getJSON('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data) {
-  $('DIV#hello').append(data.hello);
+$(document).ready(function () {
+  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data) {
+    $('DIV#hello').text(data.hello);
+  });
 });
